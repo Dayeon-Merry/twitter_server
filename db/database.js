ceab9693c0c3c50d6b7194358c94b8f03c2b5bb1
Dayeon-Merry <131240376+Dayeon-Merry@users.noreply.github.com>
2023-05-17 13:20:08 +0900

    트위터 서버 업로드

---
+++ db/database.js
@@ -0,0 +1,13 @@
+// import mysql from 'mysql2'; sequelize 깔았으므로 삭제해도 됨
+import { config } from '../config.js';
+import SQ from 'sequelize';
+
+const { host, user, database, password } = config.db;
+
+export const sequelize = new SQ.Sequelize(database, user, password, {
+    host,
+    dialect: 'mariadb',
+    logging: false,
+    timezone: "+09:00",
+    port: '31229'
+})
